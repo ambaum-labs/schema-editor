d2d56553318e00bda41cfe316d162ec6dd4cd08e
Matthew Peters <matt@ambaum.com>
2023-04-15 18:22:01 -0500

    Make tabs full width

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -17,11 +17,11 @@ export default {
 </script>
 
 <template>
-  <div class="flex mb-5">
+  <div class="flex mb-5 w-full">
     <button 
       v-for="(tab, i) in tabs"
       :class="classes(i, tab.value)"
-      class="py-2 px-5 font-semibold border-slate-800"
+      class="py-2 px-5 flex-1 font-semibold border-slate-800"
       @click="$emit('change', tab.value)"
     >
       {{ tab.title }}
